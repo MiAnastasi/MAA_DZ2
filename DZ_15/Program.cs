8ec09511704c0a4f0398ac68c8052adcac25dd17
NastyaM <Nastya@mail.com>
2022-08-22 10:36:27 +0700

    DZ#15

---
+++ DZ_15/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите число: ");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+if (daynumber >5) Console.WriteLine("да");
+else Console.WriteLine("нет");

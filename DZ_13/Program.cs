5d50a2006d9c3be1366f0e4be8a357a82555bbd9
NastyaM <Nastya@mail.com>
2022-08-22 10:37:11 +0700

    DZ#13

--- DZ_13/Program.cs
+++ DZ_13/Program.cs
@@ -5,6 +5,6 @@
 
 Console.Write("Введите число: ");
 string? number = Console.ReadLine();
-int x = Convert.ToInt32(number);
-if (x>99) Console.WriteLine("третья цифра: " + number[2]);
+int thidnumber = Convert.ToInt32(number);
+if (thidnumber>99) Console.WriteLine("третья цифра: " + number[2]);
 else Console.WriteLine("третьей цифры нет");
